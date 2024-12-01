a181a2ec2707309d2567f5808f494a58aa6770d1
Gabe Susman <gabe.susman@gmail.com>
2024-12-01 13:19:14 -0700

    first commit

---
+++ argus-ios-client/argus_ios_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  argus_ios_clientApp.swift
+//  argus-ios-client
+//
+//  Created by Max Gabriel Susman on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct argus_ios_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ argus-ios-clientTests/argus_ios_clientTests.swift
@@ -0,0 +1,17 @@
+//
+//  argus_ios_clientTests.swift
+//  argus-ios-clientTests
+//
+//  Created by Max Gabriel Susman on 12/1/24.
+//
+
+import Testing
+@testable import argus_ios_client
+
+struct argus_ios_clientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
